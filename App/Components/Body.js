ad2689bb01d5ac74b8ee7f5ad5fe0dee75162268
Carlos Perez <carlos@ioncom.com.mx>
2018-06-29 16:48:28 -0500

    Se cargan los nuevos iconos y se cmoienza la modificacion del home

--- App/Components/Body.js
+++ App/Components/Body.js
@@ -1,7 +1,4 @@
 import React, {Component} from 'react'
-var totalHeiHeight = Dimensions.get('window').height;
-var thirdWidth = totalWidth * .334;
-var totalWidth = Dimensions.get('window').width;
 import{
   Text,
   View,
@@ -11,6 +8,15 @@ import{
   Alert,
   Image
 } from 'react-native'
+import ImageView from "./ImageView";
+
+var totalHeight = Dimensions.get('window').height;
+var totalWidth = Dimensions.get('window').width;
+var thirdWidth = totalWidth * .334;
+
+console.log("heaight : ", totalHeight);
+console.log("width : ", totalWidth);
+console.log("widht /3 :" , totalWidth/3)
 
 class Body extends Component{
 
@@ -19,133 +25,142 @@ class Body extends Component{
     const{navigate} = this.props.navigation;
 
     return(
-      <View>
-        <TouchableHighlight underlayColor="#E0214F" onPress={() => navigate('Notificaciones')}>
+
+	  <View style={styles.mainContainer}>
+
+		  {/* Imagen principal */}
+		  <Image
+			  style={styles.mainImage}
+			  source={require('../Img/Home/image-home.png')}
+		  />
+
+		  {/* Primer módulo */}
+		  <View style={styles.firstModule}>
+
+              {/* Notificaciones */}
+              <TouchableHighlight underlayColor="#E0214F" onPress={() => navigate('Notificaciones')}>
+                  <View style={[styles.firstModuleIcon, styles.buttonNotifications]}>
+                      <Image
+                          source={require('../Img/Home/notificaciones.png')}
+                      />
+                  </View>
+              </TouchableHighlight>
+
+              {/* Plan Comercial */}
+              <TouchableHighlight underlayColor="#E0214F" onPress={() => navigate('Notificaciones')}>
+                  <View style={[styles.firstModuleIcon, styles.buttonPlanComercial]}>
+                      <Image
+                          source={require('../Img/Home/plancomercial.png')}
+                      />
+                  </View>
+              </TouchableHighlight>
+
+              {/* Herramientas */}
+              <TouchableHighlight underlayColor="#E0214F" onPress={() => navigate('Notificaciones')}>
+                  <View style={[styles.firstModuleIcon, styles.buttonTools]}>
+                      <Image
+                          style={styles.icons}
+                          source={require('../Img/Home/herramientas.png')}
+                      />
+                  </View>
+              </TouchableHighlight>
+          </View>
+
+          {/* Segundo módulo */}
+
           <Image
-            style={styles.homeBlock}
-            source={require('../Img/Body/notificacioneseditados2.jpg')}
+              source={require('../Img/Home/barra-connect.png')}
           />
-        </TouchableHighlight>
-        <TouchableHighlight underlayColor="#E0214F" onPress={() => navigate('PlanComercial')}>
-          <Image
-            style={styles.homeBlock}
-            source={require('../Img/Body/planco-plecaeditados2.jpg')}
-          />
-          </TouchableHighlight>
-          <TouchableHighlight underlayColor="#E0214F" onPress={() => navigate('Newsletter')}>
-            <Image
-            style={styles.homeBlock}
-            source={require('../Img/Body/herramientaseditados2.jpg')}
-            />
-          </TouchableHighlight>
-          {/*Menu de bloques*/}
-          {/*Hot-Results*/}
-          <View style={styles.horizontalHomeBlock}>
-            <TouchableHighlight underlayColor="#E0214F" onPress={() => navigate('Ibooks')}>
-              <View style={[styles.horizontalImageSection, {backgroundColor: '#F5AB2D'}]}>
-                <View style={styles.imageContentSection}>
-                  <Image
-                    source={require('../Img/Body/Icons/hotresults.png')}
-                  />
-                </View>
+
+          {/* Tercer módulo */}
+          <View style={styles.firstModule}>
+              <View style={styles.blockMenu}>
+
+                  {/* Catalogos */}
+                  <TouchableHighlight underlayColor="#E0214F" onPress={() => navigate('Catalogos')}>
+                      <View style={styles.menuModule}>
+                          <Image
+                              source={require('../Img/Home/icono-catalogo.png')}
+                          />
+                      </View>
+                  </TouchableHighlight>
+
+                  {/* Presentaciones */}
+                  <TouchableHighlight underlayColor="#E0214F" onPress={() => navigate('Presentaciones')}>
+                      <View style={styles.menuModule}>
+                          <Image
+                              source={require('../Img/Home/icono-presentaciones.png')}
+                          />
+                      </View>
+                  </TouchableHighlight>
+
+                  {/*Videos*/}
+                  <TouchableHighlight underlayColor="#2F284B" onPress={() => navigate('Videos')}>
+                      <View style={styles.menuModule}>
+                          <Image
+                              source={require('../Img/Home/icono-videos.png')}
+                          />
+                      </View>
+                  </TouchableHighlight>
+
+
               </View>
-            </TouchableHighlight>
-            {/*Calendarios*/}
-            <TouchableHighlight underlayColor="#4883C4" onPress={() => navigate('Calendario')}>
-              <View style={[styles.horizontalImageSection, {backgroundColor: '#4883C4'}]}>
-                <View style={styles.imageContentSection}>
-                  <Image
-                    source={require('../Img/Body/Icons/calendario-text.png')}
-                  />
-                </View>
-              </View>
-            </TouchableHighlight >
-            {/*Videos*/}
-            <TouchableHighlight underlayColor="#2F284B" onPress={() => navigate('Videos')}>
-              <View style={[styles.horizontalImageSection, {backgroundColor: '#2F284B'}]}>
-                <View style={styles.imageContentSection}>
-                  <Image
-                    source={require('../Img/Body/Icons/videos-text.png')}
-                  />
-                </View>
-              </View>
-            </TouchableHighlight>
           </View>
-          <View style={styles.horizontalHomeBlock}>
-            {/* Networks */}
-            <TouchableHighlight underlayColor="#E0214F" onPress={() => navigate('Networks')}>
-              <View style={[styles.horizontalImageSection, {backgroundColor: '#631158'}]}>
-                <View style={styles.imageContentSection}>
-                  <Image
-                    source={require('../Img/Body/Icons/networks-text.png')}
-                  />
-                </View>
-              </View>
-            </TouchableHighlight>
-            {/* Regional */}
-            <TouchableHighlight underlayColor="#2F284B" onPress={() => navigate('Regional')}>
-              <View style={[styles.horizontalImageSection, {backgroundColor: '#e74655'}]}>
-                <View style={styles.imageContentSection}>
-                  <Image
-                    source={require('../Img/Body/Icons/icon-regional.png')}
-                  />
-                </View>
-              </View>
-            </TouchableHighlight>
-            {/* Parrillas */}
-            <TouchableHighlight underlayColor="#2F284B" onPress={() => navigate('Parrillas')}>
-              <View style={[styles.horizontalImageSection, {backgroundColor: '#F5AB2D'}]}>
-                <View style={styles.imageContentSection}>
-                  <Image
-                    source={require('../Img/Body/Icons/icon-parrllas.png')}
-                  />
-                </View>
-              </View>
-            </TouchableHighlight>
-          </View>
-          <View style={styles.horizontalHomeBlockEnd}>
-            {/* Presentaciones */}
-            <TouchableHighlight underlayColor="#E0214F" onPress={() => navigate('Presentaciones')}>
-              <View style={[styles.horizontalImageSectionEnd, {backgroundColor: '#2F284B'}]}>
-                <View style={styles.imageContentSection}>
-                  <Image
-                    source={require('../Img/Body/Icons/presentaciones.png')}
-                  />
-                </View>
-              </View>
-            </TouchableHighlight>
-            {/* Catalogos */}
-            <TouchableHighlight underlayColor="#E0214F" onPress={() => navigate('Catalogos')}>
-              <View style={[styles.horizontalImageSectionEnd, {backgroundColor: '#3581BC'}]}>
-                <View style={styles.imageContentSection}>
-                  <Image
-                    source={require('../Img/Body/Icons/catalogos.png')}
-                  />
-                </View>
-              </View>
-            </TouchableHighlight>
-          </View>
-      </View>
-    )
+	  </View>
+	)
   }
 }
 
 var styles = StyleSheet.create({
+
+    mainImage:{
+      width:totalWidth,
+    },
+    firstModule:{
+        flexDirection: 'row',
+    },
+    blockMenu:{
+        flexDirection: 'row',
+    },
+    firstModuleIcon:{
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: (totalWidth / 3),
+        height: (totalHeight * .07),
+    },
+    menuModule:{
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: (totalWidth / 3),
+    },
+
+
+
+    buttonNotifications:{
+        backgroundColor: '#2F5062',
+    },
+    buttonTools:{
+        backgroundColor: '#2F5062',
+    },
+    buttonPlanComercial:{
+        backgroundColor:'#036666',
+    },
+
   homeBlock:{
-   height: totalHeiHeight*.1500,
+   height: totalHeight*.1500,
    width: totalWidth,
    marginTop: -1,
   },
   horizontalHomeBlock:{
     justifyContent: 'center',
     flexDirection: 'row',
-    height: totalHeiHeight*.137,
+    height: totalHeight*.137,
     width: totalWidth,
   },
   horizontalImageSection:{
    flexDirection: 'row',
    justifyContent: 'center',
-   height: totalHeiHeight*.137,
+   height: totalHeight*.137,
    width: totalWidth*.334,
    backgroundColor: 'black'
   },
@@ -155,13 +170,13 @@ var styles = StyleSheet.create({
   },
   horizontalHomeBlockEnd:{
     flexDirection: 'row',
-    height: totalHeiHeight*.137,
+    height: totalHeight*.137,
     width: totalWidth,
   },
   horizontalImageSectionEnd:{
    flexDirection: 'row',
    justifyContent: 'center',
-   height: totalHeiHeight*.137,
+   height: totalHeight*.137,
    width: totalWidth*.500,
    backgroundColor: 'black'
   }
